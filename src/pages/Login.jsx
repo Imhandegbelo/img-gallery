8d6669c3d4591943c78dcb4dbe75c752bfdac2ee
Imhandegbelo <abramgeo5@gmail.com>
2023-09-22 01:13:39 +0100

    fixed: login redirect

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,17 +7,37 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [errorMessage, setErrorMessage] = useState({});
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [emailTouched, setEmailTouched] = useState(false);
+  const [passTouched, setPassTouched] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
   const _email = "user@example.com";
   const _password = "1Password";
   const navigate = useNavigate();
+  const errors = {
+    email: ["Invalid username", "Email cannot be empty"],
+    pass: [
+      "Invalid Password",
+      "Password cannot be empty",
+      "Password must be at least 8 characters long",
+    ],
+  };
+
+  function renderErrorMessage(message) {
+    return (
+      <small className="text-sm md:text-base text-rose-500">{message}</small>
+    );
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSubmitted(true);
+
     console.log(email);
     console.log(password);
-    if(email === _email && password === _password){
-      setIsLoggedIn(true)
-      navigate("/home")
+
+    if (email === _email && password === _password) {
+      setIsLoggedIn(true);
+      navigate("/home");
     }
   }
 
@@ -33,15 +53,21 @@ export default function Login() {
             Email
             <input
               autoFocus
+              required
               type="email"
               id="email"
               name="email"
               placeholder="me@email.com"
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                setEmailTouched(true);
+              }}
               className="rounded px-4 py-3 w-full mt-1 bg-white text-gray-900 border border-gray-200 focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-100"
             />
           </label>
-
+          {submitted && email === ""
+            ? renderErrorMessage("Email cannot be blank")
+            : ""}
           <label className="text-gray-500 block mt-3">
             Password
             <input
@@ -54,7 +80,9 @@ export default function Login() {
               className="rounded px-4 py-3 w-full mt-1 bg-white text-gray-900 border border-gray-200 focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-100"
             />
           </label>
-
+          {submitted && password === ""
+            ? renderErrorMessage("Password cannot be blank")
+            : ""}
           <button
             onClick={() => preventDefault(e)}
             className="mt-6 transition transition-all block py-3 px-4 w-full text-white font-bold rounded cursor-pointer bg-gradient-to-r from-indigo-600 to-purple-400 hover:from-indigo-700 hover:to-purple-500 focus:bg-indigo-900 transform hover:-translate-y-1 hover:shadow-lg"

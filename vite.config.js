0ad04862bb9ab2c791e117ef125f625921cab781
Imhandegbelo <abramgeo5@gmail.com>
2024-02-08 05:51:00 +0100

    fix: env variables with vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: process.env.VITE_VERCEL_ENV,
+      __API_KEY__: process.env.VITE_apiKey,
+      __authDomain__: process.env.VITE_authDomain,
+      __projectId__: process.env.VITE_projectId,
+      __storageBucket__: process.env.VITE_storageBucket,
+      __messagingSenderId__: process.env.VITE_messagingSenderId,
+      __appId__: process.env.VITE_appId,
+      __unsplashKey__: process.env.VITE_unsplashKey,
+    },
+  };
+});
